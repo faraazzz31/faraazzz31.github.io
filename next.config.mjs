2fe0860895e30c8a9c1815f83976cc8248e0a9a8
faraazzz31 <faraaz.ahmed31@gmail.com>
2024-11-13 01:09:40 -0500

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/faraazzz31.github.io', // Must match your repository name exactly
+    assetPrefix: '/faraazzz31.github.io', // Add this line
+}
+
 
 export default nextConfig;
